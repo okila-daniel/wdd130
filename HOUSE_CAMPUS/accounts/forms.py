8154e0650b9c53e38f7f4664951051aa074f6b11
okila-daniel <tokila@byupathway.edu>
2026-07-31 01:23:50 +0100

    mm

--- HOUSE_CAMPUS/accounts/forms.py
+++ HOUSE_CAMPUS/accounts/forms.py
@@ -4,39 +4,62 @@ from .models import User
 
 
 class StudentRegistrationForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control form-control-lg"
+
+        self.fields["password1"].widget.attrs["placeholder"] = "Password"
+        self.fields["password2"].widget.attrs["placeholder"] = "Confirm Password"
+
     class Meta:
         model = User
         fields = (
-            "username",
             "first_name",
             "last_name",
+            "username",
             "email",
             "phone",
             "whatsapp_number",
-            "university",
-            "county",
-            "town",
             "password1",
             "password2",
         )
 
         widgets = {
-            "username": forms.TextInput(attrs={"class": "form-control"}),
-            "first_name": forms.TextInput(attrs={"class": "form-control"}),
-            "last_name": forms.TextInput(attrs={"class": "form-control"}),
-            "email": forms.EmailInput(attrs={"class": "form-control"}),
-            "phone": forms.TextInput(attrs={"class": "form-control"}),
-            "whatsapp_number": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "254712345678",
-                }
-            ),
-            "university": forms.TextInput(attrs={"class": "form-control"}),
-            "county": forms.TextInput(attrs={"class": "form-control"}),
-            "town": forms.TextInput(attrs={"class": "form-control"}),
+            "first_name": forms.TextInput(attrs={
+                "placeholder": "First Name"
+            }),
+            "last_name": forms.TextInput(attrs={
+                "placeholder": "Last Name"
+            }),
+            "username": forms.TextInput(attrs={
+                "placeholder": "Username"
+            }),
+            "email": forms.EmailInput(attrs={
+                "placeholder": "Email Address"
+            }),
+            "phone": forms.TextInput(attrs={
+                "placeholder": "Phone Number"
+            }),
+            "whatsapp_number": forms.TextInput(attrs={
+                "placeholder": "WhatsApp Number"
+            }),
         }
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        user.role = "student"
+        user.university = "Maasai Mara University"
+        user.county = "Narok"
+        user.town = "Narok"
+
+        if commit:
+            user.save()
+
+        return user
 
 class ProfileForm(forms.ModelForm):
     class Meta:
--- HOUSE_CAMPUS/housecampus/settings.py
+++ HOUSE_CAMPUS/housecampus/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-kyyq$gh_*dpy%y0(p#*xwrqeaj+m+!+q%&c5(r!_&qhos)o6j8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
