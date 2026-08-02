f728ed10c3f3ce9a96acd3ca3de284a1059c2adc
okila-daniel <tokila@byupathway.edu>
2026-08-02 19:38:25 +0100

    Configure Cloudinary storage

--- HOUSE_CAMPUS/housecampus/settings.py
+++ HOUSE_CAMPUS/housecampus/settings.py
@@ -246,14 +246,12 @@ LOGOUT_REDIRECT_URL = "home"
 # -----------------------------------------------------------------------------
 
 import os
-import cloudinary
 
-cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-    secure=True,
-)
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
 
 STORAGES = {
     "default": {
