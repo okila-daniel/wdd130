d544a3e83fde1f1972331437f2fa05a04de696d9
okila-daniel <tokila@byupathway.edu>
2026-08-02 19:46:15 +0100

    MM

--- HOUSE_CAMPUS/housecampus/settings.py
+++ HOUSE_CAMPUS/housecampus/settings.py
@@ -262,6 +262,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
