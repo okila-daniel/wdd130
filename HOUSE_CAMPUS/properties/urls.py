c99023f15baf33d23da697d071de8b0a39d5fb47
okila-daniel <tokila@byupathway.edu>
2026-08-02 21:52:04 +0100

    mm

--- HOUSE_CAMPUS/properties/urls.py
+++ HOUSE_CAMPUS/properties/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "properties"
+
 urlpatterns = [
     path("", views.property_list, name="property_list"),
     path("add/", views.add_property, name="add_property"),
