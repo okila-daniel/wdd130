cd3d222e5cc337dae7358f6049eb1f83c1efba7a
okila-daniel <tokila@byupathway.edu>
2026-08-02 22:07:00 +0100

    mm

--- HOUSE_CAMPUS/portal/urls.py
+++ HOUSE_CAMPUS/portal/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "portal"
+
 urlpatterns = [
-    path("", views.dashboard_home, name="dashboard"),
+    path("", views.dashboard_home, name="home"),
 ]
